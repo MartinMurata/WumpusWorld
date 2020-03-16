94b805f90735380914e28eb755cf500cf1ae09bd
Martin Murata <mpmurata@uci.edu>
2020-03-16 12:53:05 -0700

    making some progress

--- Wumpus_World_Python_Shell/src/MyAI.py
+++ Wumpus_World_Python_Shell/src/MyAI.py
@@ -36,92 +36,92 @@ class MyAI ( Agent ):
         self.moveStack = []
         self.previousTile = (0,0)
         self.currentTile = (0,0) 
-        self.targetTile = (0,0) 
+        self.targetTile = (0,1) 
         self.facing = Direction.RIGHT 
         self.findGoldState = True 
         self.goHomeState = False 
+        self.onGoldState = False
+        self.isTurning = False
         self.possibleMapSize = [8,8] 
 
     def getAction( self, stench, breeze, glitter, bump, scream ):
-
-    def findingGoldAction( self, glitter):
-
-    def goHomeAction(self, stench, breeze, glitter, bump, scream ):
+        if not self.isTurning:
+            self.updateWorld(stench, breeze, glitter, bump, scream)
+        if self.onGoldState:
+            self.onGoldState = False
+            self.findGoldState = False
+            self.goHomeState = True
+            return Agent.Action.GRAB
+        if self.findGoldState and not self.isTurning:
+            self.setTargetTile()
+        if self.goHomeState and not self.isTurning:
+            self.setGoHomeTile()    
+        return self.moveToTargetTile()
 
     def setGoHomeTile(self):
+        if len(self.moveStack) > 0:
+            self.targetTile = self.moveStack.pop()
 
-    def setTargetTile(self): # set the next Target Tile
+    def setTargetTile(self):
+        unvisitedAdjTiles = [tile for tile in self.safeMap[self.currentTile] if tile not in self.visited]
+        if len(unvisitedAdjTiles) > 1:
+            self.targetTile = unvisitedAdjTiles[random.randint(0,len(unvisitedAdjTiles)-1)]
+        elif len(unvisitedAdjTiles) == 1:
+            self.targetTile = unvisitedAdjTiles[0]
+        elif len(self.moveStack) > 0:
+            print('BACKTRACK')
+            self.targetTile = self.moveStack.pop()
+            print(f'new target: {self.targetTile}')
+        else:
+            self.goHomeState = True
+            self.findGoldState = False
+        print(f'visited tiles{self.visited}')
+        print(f'UNVISITED ADJ TILES {unvisitedAdjTiles}')
 
-    def updateWorld(self, stench, breeze, bump, scream):
+    def updateWorld(self, stench, breeze, glitter, bump, scream):
+        if glitter:
+            self.handleGlitter()
+            return
+        if not breeze and not stench and not bump:
+            self.addSafe()
+        if breeze and self.currentTile == (0,0):
+            self.onGoldState = False
+            self.findGoldState = False
+            self.goHomeState = True
+            return
+        if stench:
+            self.handleStench()
+        if bump:
+            self.handleBump()
+        if scream:
+            self.handleScream()
+        self.markVisited()
+        
+    def handleGlitter(self):
+        self.onGoldState = True
 
-    def moveToTargetTile(self):
-        '''update the face and current tile before moving forward'''
-        if (self.targetTile[0] > self.currentTile[0]) and (self.targetTile[1] == self.currentTile[1]):
-            if self.facing == Direction.RIGHT:
-                self.currentTile = self.targetTile
-                return Agent.Action.FORWARD
-            elif self.facing == Direction.DOWN:
-                self.facing = Direction.RIGHT
-                return Agent.Action.TURN_LEFT
-            elif self.facing == Direction.UP:
-                self.facing = Direction.RIGHT
-                return Agent.Action.TURN_RIGHT
-            elif self.facing == Direction.LEFT: 
-                self.facing = Direction.UP
-                return Agent.Action.TURN_RIGHT
+    def handleStench(self):
+        return Agent.Action.SHOOT
 
-        elif (self.targetTile[0] == self.currentTile[0]) and (self.targetTile[1] > self.currentTile[1]): 
-            if self.facing == Direction.UP:
-                self.previousTile = self.currentTile
-                self.currentTile = self.targetTile
-                return Agent.Action.FORWARD
-            elif self.facing == Direction.RIGHT:
-                self.facing = Direction.UP
-                return Agent.Action.TURN_LEFT
-            elif self.facing == Direction.LEFT:
-                self.facing = Direction.UP
-                return Agent.Action.TURN_RIGHT
-            elif self.facing == Direction.DOWN:
-                # need to face left first
-                self.facing = Direction.LEFT
-                return Agent.Action.TURN_RIGHT
-            
-        elif (self.targetTile[0] < self.currentTile[0]) and (self.targetTile[1] == self.currentTile[1]):
-            if self.facing == Direction.LEFT:
-                self.previousTile = self.currentTile
-                self.currentTile = self.targetTile
-                return Agent.Action.FORWARD
-            elif self.facing == Direction.UP:
-                self.facing = Direction.LEFT
-                return Agent.Action.TURN_LEFT
-            elif self.facing == Direction.DOWN:
-                self.facing = Direction.LEFT
-                return Agent.Action.TURN_RIGHT
-            elif self.facing == Direction.RIGHT:
-                self.facing = Direction.DOWN
-                return Agent.Action.TURN_RIGHT
+    def handleScream(self):
+        self.addSafe()
 
-        elif (self.targetTile[0] == self.currentTile[0]) and (self.targetTile[1] < self.currentTile[1]):
-            if self.facing == Direction.DOWN:
-                self.previousTile = self.currentTile
-                self.currentTile = self.targetTile
-                return Agent.Action.FORWARD
-            elif self.facing == Direction.LEFT:
-                self.facing = Direction.DOWN
-                return Agent.Action.TURN_LEFT
-            elif self.facing == Direction.RIGHT:
-                self.facing = Direction.DOWN
-                return Agent.Action.TURN_RIGHT
-            elif self.facing == Direction.UP:
-                self.facing = Direction.RIGHT
-                return Agent.Action.TURN_RIGHT
+    def markVisited(self):
+        self.visited.add(self.currentTile)
+        if self.currentTile not in self.moveStack:
+            self.moveStack.append(self.currentTile)
 
+    def addSafe(self):
+        for tile in self.adjTiles():
+            self.safeMap[self.currentTile].add(tile)
 
-    def updateWalls(self):
+    def handleBump(self):
         if self.facing == Direction.UP:
             self.possibleMapSize[1] = self.currentTile[1]
         elif self.facing == Direction.RIGHT:
             self.possibleMapSize[0] = self.currentTile[0]
+        # self.visited.remove(self.currentTile)
+        self.currentTile = self.moveStack.pop()
 
     def adjTiles(self):
         adjT = []
@@ -135,3 +135,79 @@ class MyAI ( Agent ):
             if (0 <= tile[0] < self.possibleMapSize[0] and 0 <= tile[1] < self.possibleMapSize[1]):
                 adjT.append(tile)
         return adjT
+
+    def moveToTargetTile(self): 
+        if self.currentTile == (0,0) and self.goHomeState:
+            return Agent.Action.CLIMB
+        if (self.targetTile[0] > self.currentTile[0]) and (self.targetTile[1] == self.currentTile[1]):
+            if self.facing == Direction.RIGHT:
+                self.isTurning = False
+                self.currentTile = self.targetTile
+                return Agent.Action.FORWARD
+            elif self.facing == Direction.DOWN:
+                self.isTurning = True
+                self.facing = Direction.RIGHT
+                return Agent.Action.TURN_LEFT
+            elif self.facing == Direction.UP:
+                self.isTurning = True
+                self.facing = Direction.RIGHT
+                return Agent.Action.TURN_RIGHT
+            elif self.facing == Direction.LEFT:
+                self.isTurning = True 
+                self.facing = Direction.UP
+                return Agent.Action.TURN_RIGHT
+
+        elif (self.targetTile[0] == self.currentTile[0]) and (self.targetTile[1] > self.currentTile[1]): 
+            if self.facing == Direction.UP:
+                self.isTurning = False
+                self.currentTile = self.targetTile
+                return Agent.Action.FORWARD
+            elif self.facing == Direction.RIGHT:
+                self.isTurning = True
+                self.facing = Direction.UP
+                return Agent.Action.TURN_LEFT
+            elif self.facing == Direction.LEFT:
+                self.isTurning = True
+                self.facing = Direction.UP
+                return Agent.Action.TURN_RIGHT
+            elif self.facing == Direction.DOWN:
+                self.isTurning = True
+                # need to face left first
+                self.facing = Direction.LEFT
+                return Agent.Action.TURN_RIGHT
+            
+        elif (self.targetTile[0] < self.currentTile[0]) and (self.targetTile[1] == self.currentTile[1]):
+            if self.facing == Direction.LEFT:
+                self.isTurning = False
+                self.currentTile = self.targetTile
+                return Agent.Action.FORWARD
+            elif self.facing == Direction.UP:
+                self.isTurning = True
+                self.facing = Direction.LEFT
+                return Agent.Action.TURN_LEFT
+            elif self.facing == Direction.DOWN:
+                self.isTurning = True
+                self.facing = Direction.LEFT
+                return Agent.Action.TURN_RIGHT
+            elif self.facing == Direction.RIGHT:
+                self.isTurning = True
+                self.facing = Direction.DOWN
+                return Agent.Action.TURN_RIGHT
+
+        elif (self.targetTile[0] == self.currentTile[0]) and (self.targetTile[1] < self.currentTile[1]):
+            if self.facing == Direction.DOWN:
+                self.isTurning = False
+                self.currentTile = self.targetTile
+                return Agent.Action.FORWARD
+            elif self.facing == Direction.LEFT:
+                self.isTurning = True
+                self.facing = Direction.DOWN
+                return Agent.Action.TURN_LEFT
+            elif self.facing == Direction.RIGHT:
+                self.isTurning = True
+                self.facing = Direction.DOWN
+                return Agent.Action.TURN_RIGHT
+            elif self.facing == Direction.UP:
+                self.isTurning = True
+                self.facing = Direction.RIGHT
+                return Agent.Action.TURN_RIGHT
